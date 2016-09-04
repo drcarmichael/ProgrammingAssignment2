0333cc320ee81c14aca97b95a08530cbc36b52b4
drcarmichael <martin@odo.net>
2016-09-04 12:30:48 -0700

    new origin commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  
+## While the objective of this is supposed to be the time saving element
+## I think this is about proding a list of functions, and calling function
+## as a list. the make fucniton returns a list with the elements
+## set a matrix, get a matrix, set an inverse, gent an inverse
+## flg is a flag to indicate that the cashe is set
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   mat <- NULL          ## local matrix, which is also a flag
+   set <- function(y) { 
+       x <<-  y          ##  return matrix global, new matrix to work
+	   mat <<- NULL     ##    set global mat to Null, so set inv mat to NULL
+    }
+	get <- function() { x  }   ##   get the matrix passed in. single line func
+    setinv <- function(inv) { mat <<- inv }  ## Set the inverse, publish it
+	getinv <- function() { mat }    ## return the current inverted  mat
+# Create our function list
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+##  this is where we use our clever 'list-function' utility. I would guess
+##  this reinforces the idea that eveything in R is a function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#  check  a matrix that is the inverse of 'x'
+	mat <- x$getinv()     # get our inverted matrix
+    if(!is.null(mat)) { return(mat) }    ## if not null then we are done
+	mat <- x$get()
+	mat <- solve(mat,...)
+	x$setinv(mat)
+	return(mat)
 }
